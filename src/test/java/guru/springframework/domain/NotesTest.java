a0d183bb7d6843695b8969c38ec0fbec31bf67a0
lentiummmx <lentium.mmx@gmail.com>
2021-04-29 15:16:08 -0500

    junit-4-legacy-app File(s) committed to adding junit-vintage and junit5 libs (v0.1.0)

---
+++ src/test/java/guru/springframework/domain/NotesTest.java
@@ -0,0 +1,19 @@
+package guru.springframework.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NotesTest {
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void getId() {
+        //assertEquals(1, 3);
+        assertEquals(3, 3);
+    }
+}
